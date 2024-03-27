91a3dccee2f2a74896aa7a1c8b5ae9e498756bfd
Ri Teii <teiiri@outlook.com>
2024-03-27 22:54:11 +0000

    🐣 Add new modules `risc_v`

---
+++ src/risc_v.sv
@@ -0,0 +1,111 @@
+`include "instruction_memory.sv"
+`include "reg_file.sv"
+`include "extend.sv"
+`include "alu.sv"
+`include "data_memory_and_io.sv"
+`include "program_counter.sv"
+`include "control_unit.sv"
+
+module risc_v (
+    output logic [31:0] CPUOut,
+    input logic [31:0] CPUIn,
+    input logic Reset,
+    input logic CLK
+);
+
+    // Control Signals
+    logic Ctl_RegWrite, Ctl_MemWrite, Ctl_AluSrc;
+    logic [1:0] Ctl_ResultSrc, Ctl_PcSrc;
+    logic [2:0] Ctl_AluMode, Ctl_ImmSrc;
+
+    // PC I/O
+    logic [31:0] PcCurrent, PcNext, PcImmTarget;
+
+    // ALU I/O
+    logic [31:0] AluSrcB, AluResult;
+    logic AluZero;
+
+    // Register I/O
+    logic [4:0] RegAddrR1, RegAddrR2, RegAddrW;
+    logic [31:0] RegDataR1, RegDataR2;
+
+    // Others
+    logic [31:0] Instr, ExtendedImm, MemDataR, Result;
+
+    assign RegAddrR1 = Instr[19:15];
+    assign RegAddrR2 = Instr[24:20];
+    assign RegAddrW = Instr[11:7];
+    assign AluSrcB = (Ctl_AluSrc) ? ExtendedImm : RegDataR2;
+    assign PcImmTarget = PcCurrent + ExtendedImm;
+
+    always_comb begin
+        case (Ctl_ResultSrc)
+            2'b00: Result = AluResult;
+            2'b01: Result = MemDataR;
+            2'b10: Result = PcNext;
+        endcase
+    end
+
+    program_counter PC (
+        .PC(PcCurrent),
+        .PCPlus4(PcNext),
+        .PCTarget(PcImmTarget),
+        .ALUResult(AluResult),
+        .PCSrc(Ctl_PcSrc),
+        .Reset(Reset),
+        .CLK(CLK)
+    );
+
+    instruction_memory InstMem (
+        .Instr(Instr),
+        .PC(PcCurrent)
+    );
+
+    control_unit ControlUnit (
+        .PCSrc(Ctl_PcSrc),
+        .ResultSrc(Ctl_ResultSrc),
+        .MemWrite(Ctl_MemWrite),
+        .ALUSrc(Ctl_AluSrc),
+        .RegWrite(Ctl_RegWrite),
+        .ALUControl(Ctl_AluMode),
+        .ImmSrc(Ctl_ImmSrc),
+        .Instr(Instr),
+        .Zero(AluZero)
+    );
+
+    reg_file RegFile (
+        .RD1(RegDataR1),
+        .RD2(RegDataR2),
+        .WD3(Result),
+        .A1 (RegAddrR1),
+        .A2 (RegAddrR2),
+        .A3 (RegAddrW),
+        .WE3(Ctl_RegWrite),
+        .CLK(CLK)
+    );
+
+    extend Ext (
+        .ImmExt(ExtendedImm),
+        .Instr (Instr),
+        .ImmSrc(Ctl_ImmSrc)
+    );
+
+    alu ALU (
+        .ALUResult(AluResult),
+        .Zero(AluZero),
+        .SrcA(RegDataR1),
+        .SrcB(AluSrcB),
+        .ALUControl(Ctl_AluMode)
+    );
+
+    data_memory_and_io MemIO (
+        .RD(MemDataR),
+        .CPUOut(CPUOut),
+        .A(AluResult),
+        .WD(RegDataR2),
+        .CPUIn(CPUIn),
+        .WE(Ctl_MemWrite),
+        .CLK(CLK)
+    );
+
+endmodule
---
+++ test/risc_v_tb.sv
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+`include "risc_v.sv"
+
+module risc_v_tb;
+    logic [31:0] CPUOut, CPUIn;
+    logic Reset, CLK;
+
+    logic [31:0] pc_prev;
+
+    risc_v dut (
+        CPUOut,
+        CPUIn,
+        Reset,
+        CLK
+    );
+
+    // Generate clock signal with 20 ns period
+    initial begin
+        CLK = 0;
+        forever #10 CLK = ~CLK;
+    end
+
+    // Exit simulation when the PC stops changing
+    always @(posedge CLK) begin
+        if (dut.PcCurrent == pc_prev) $finish;
+        pc_prev = dut.PcCurrent;
+    end
+
+    // Print CPU state
+    always @(negedge CLK)
+        $display(
+            "t = %3d, CPUIn = %d, CPUOut = %d, Reset = %b, PC = %d, ALUResult = %d",
+            $time,
+            CPUIn,
+            CPUOut,
+            Reset,
+            dut.PcCurrent,
+            dut.AluResult
+        );
+
+    // Dump VCD file
+    initial begin
+        $dumpfile("risc_v_tb.vcd");
+        $dumpvars(0, risc_v_tb);
+    end
+
+endmodule
